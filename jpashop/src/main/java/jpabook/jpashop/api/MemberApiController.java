989547a9eccbe10dbfa363053fcca51019e04365
dev-splin <dev.splin@gmail.com>
2022-12-15 14:23:22 +0900

    ADD : [Spring Boot] API 개발 기본 - 회원 수정 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,12 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -32,6 +30,14 @@ public class MemberApiController {
         return new CreateMemberResponse(joinId);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest updateMemberRequest) {
+        memberService.update(id, updateMemberRequest.getName());
+        Member findMember = memberService.findOne(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
     @Data
     private static class CreateMemberResponse {
         private Long id;
@@ -43,6 +49,18 @@ public class MemberApiController {
 
     @Data
     private static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    private static class UpdateMemberRequest {
         private String name;
     }
 }
